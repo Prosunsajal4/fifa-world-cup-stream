1f25d11ed1ce839800e7512652eb512421e014a3
Sajal Prosun <prosunsajal123@gmail.com>
2026-06-30 11:50:28 +0600

    Replace geo-restricted hoichoi24 with proxy-reachable sources (bozztv, cloudfront, aynaott)

--- src/app/api/iptv/route.ts
+++ src/app/api/iptv/route.ts
@@ -10,22 +10,16 @@ interface Channel {
 }
 
 const BD_CHANNELS: Channel[] = [
-  { name: "Maasranga TV", url: "https://cdn.hoichoi24.com/lb/maasrangatv/maasranga_720p/playlist.m3u8", logo: "", group: "bd" },
-  { name: "Gazi TV", url: "https://cdn.hoichoi24.com/lb/gazitv/gazitv_720p/playlist.m3u8", logo: "", group: "bd" },
-  { name: "T Sports HD", url: "https://cdn.hoichoi24.com/lb/tsports/tsports_720p/playlist.m3u8", logo: "", group: "bd" },
-  { name: "BTV", url: "https://cdn.hoichoi24.com/lb/btv/btv_360p/playlist.m3u8", logo: "", group: "bd" },
-  { name: "BTV World", url: "https://cdn.hoichoi24.com/lb/btv/btv_720p/playlist.m3u8", logo: "", group: "bd" },
-  { name: "Ekattor TV", url: "https://cdn.hoichoi24.com/lb/ekattortv/ekattortv_360p/playlist.m3u8", logo: "", group: "bd" },
-  { name: "Jamuna TV", url: "https://cdn.hoichoi24.com/lb/jamunatv/jamunatv_360p/playlist.m3u8", logo: "", group: "bd" },
-  { name: "Somoy TV", url: "https://cdn.hoichoi24.com/lb/somoytv/somoytv_360p/playlist.m3u8", logo: "", group: "bd" },
-  { name: "NTV", url: "https://cdn.hoichoi24.com/lb/ntv/ntv_360p/playlist.m3u8", logo: "", group: "bd" },
-  { name: "Channel i", url: "https://cdn.hoichoi24.com/lb/channeli/channeli_360p/playlist.m3u8", logo: "", group: "bd" },
-  { name: "Channel 24", url: "https://cdn.hoichoi24.com/lb/channel24/channel24_360p/playlist.m3u8", logo: "", group: "bd" },
-  { name: "DBC News", url: "https://cdn.hoichoi24.com/lb/dbcnews/dbcnews_360p/playlist.m3u8", logo: "", group: "bd" },
-  { name: "Nagorik TV", url: "https://cdn.hoichoi24.com/lb/nagoriktv/nagoriktv_360p/playlist.m3u8", logo: "", group: "bd" },
-  { name: "Independent TV", url: "https://cdn.hoichoi24.com/lb/independent/independent_360p/playlist.m3u8", logo: "", group: "bd" },
-  { name: "Deepto TV", url: "https://cdn.hoichoi24.com/lb/deeptotv/deeptotv_360p/playlist.m3u8", logo: "", group: "bd" },
-  { name: "RTV", url: "https://cdn.hoichoi24.com/lb/rtv/rtv_360p/playlist.m3u8", logo: "", group: "bd" },
+  { name: "Jamuna TV", url: "https://bozztv.com/rongo/rongo-JamunaTelevision/index.m3u8", logo: "", group: "bd" },
+  { name: "Independent TV", url: "https://bozztv.com/rongo/rongo-IndependentTV/index.m3u8", logo: "", group: "bd" },
+  { name: "Desh TV", url: "https://bozztv.com/rongo/rongo-DeshTV/index.m3u8", logo: "", group: "bd" },
+  { name: "ATN News", url: "https://bozztv.com/rongo/rongo-ATNNews/index.m3u8", logo: "", group: "bd" },
+  { name: "BTV Chattogram", url: "https://bozztv.com/rongo/rongo-BTVChattagram/index.m3u8", logo: "", group: "bd" },
+  { name: "Madani Channel", url: "https://streaming.madanichannel.tv/static/streaming-playlists/hls/d3e49b76-ac06-4689-a641-9200445b647f/master.m3u8", logo: "", group: "bd" },
+  { name: "Peace TV Bangla", url: "https://dzkyvlfyge.erbvr.com/PeaceTvBangla/index.m3u8", logo: "", group: "bd" },
+  { name: "Azan TV", url: "https://dbcanada.sonarbanglatv.com/azantv/atv/index.m3u8", logo: "", group: "bd" },
+  { name: "Aamar Bangla", url: "https://app.ncare.live/c3VydmVyX8RpbEU9Mi8xNy8yMDE0GIDU6RgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcGVMZEJCTEFWeVN3PTOmdFsaWRtaW51aiPhnPTI/amarbanglatv.stream/playlist.m3u8", logo: "", group: "bd" },
+  { name: "Channel S", url: "https://app.ncare.live/c3VydmVyX8RpbEU9Mi8xNy8yMDE0GIDU6RgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcGVMZEJCTEFWeVN3PTOmdFsaWRtaW51aiPhnPTI/chsukoff.stream/playlist.m3u8", logo: "", group: "bd" },
 ];
 
 const INDIAN_BANGLA_CHANNELS: Channel[] = [
@@ -35,10 +29,8 @@ const INDIAN_BANGLA_CHANNELS: Channel[] = [
 ];
 
 const SPORTS_CHANNELS: Channel[] = [
-  { name: "T Sports HD", url: "https://cdn.hoichoi24.com/lb/tsports/tsports_720p/playlist.m3u8", logo: "", group: "sports" },
-  { name: "Maasranga TV", url: "https://cdn.hoichoi24.com/lb/maasrangatv/maasranga_720p/playlist.m3u8", logo: "", group: "sports" },
-  { name: "Gazi TV", url: "https://cdn.hoichoi24.com/lb/gazitv/gazitv_720p/playlist.m3u8", logo: "", group: "sports" },
   { name: "DD Sports", url: "https://d3qs3d2rkhfqrt.cloudfront.net/out/v1/b17adfe543354fdd8d189b110617cddd/index.m3u8", logo: "", group: "sports" },
+  { name: "Star Jalsha HD", url: "https://tvsen4.aynaott.com/n64PH4YL/index.m3u8", logo: "", group: "sports" },
 ];
 
 export async function GET(request: Request) {
